1bf4af0457df9f74098dc025f7e805bcb95dba64
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-12 02:29:24 +0100

    connect database

---
+++ login/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$server_name = "localhost";
+$server_user = "root";
+$server_pass = "";
+$database_name = "social_app";
+
+$verbindung = new mysqli($server_name, $server_user, $server_pass, $database_name);
+
+
+?>
