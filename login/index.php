1aa81dd33b58f66a8d6be7209bc1ad22fbcff254
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-12 02:29:39 +0100

    index editted

---
+++ login/index.php
@@ -0,0 +1,67 @@
+<?php
+    $eingeloggt = false;
+    $name = "";
+
+    if(isset($_COOKIE["eingeloggt"]) && $_COOKIE["eingeloggt"] == "1")
+    {
+        $eingeloggt = true;
+    }
+    if(!empty($_COOKIE["name"]))
+    {
+        $name = $_COOKIE["name"];
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Just Share</title>
+    <link rel="stylesheet" href="style1.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7ce396367e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="logo">
+    <img src="logo.png">
+</div>
+<div class="formContainer">
+    
+    
+    <?php
+
+
+        //   if($eingeloggt)
+        //  {
+        //     header("Location: ../Social-Media-App/index.php");
+            
+        //     exit();
+        //  }
+        //   else
+        // {
+            ?>
+                
+                    <img src="user.png">
+                    <h1>Login</h1>
+
+                    <form method="POST" action="login.php">
+                        <input type="email" placeholder="E-Mail-Address" name="email" required>
+                        <input type="password" placeholder="Password" name="pass" required>
+                        <input type="submit" value="Login">
+                    </form>
+            <div class="bottom">
+            <h5>Are you new?</h5>
+            <a href="registerPage.php">Sign In</a>
+            </div>
+            
+            <?php
+    // }
+
+    ?>
+    </div>
+    
+</body>
+</html>
